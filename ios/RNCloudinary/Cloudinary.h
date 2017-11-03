b96b281dc7b1888a51a6602091350da4cb1fe348
Andrew Gouin <agouin@singularity-interactive.com>
2017-11-03 15:04:44 -0600

    Update Cloudinary.h

--- ios/RNCloudinary/Cloudinary.h
+++ ios/RNCloudinary/Cloudinary.h
@@ -13,5 +13,5 @@
 #endif
 
 @interface Cloudinary: NSObject <RCTBridgeModule>
-+ (void) uploadChunk:(unsigned int)firstByte;
++ (void) uploadChunk:(unsigned int) firstByte mUrl: (NSString *) mUrl mParams: (NSDictionary *) mParams mData: (NSData *) mData mFilename: (NSString *) mFilename mType: (NSString *) mType mUniqueId: (NSString *) mUniqueId lastByte: (unsigned int) lastByte shouldContinue: (bool) shouldContinue mResolve: (RCTPromiseResolveBlock) mResolve mReject: (RCTPromiseRejectBlock) mReject eventDispatcher: (RCTEventDispatcher *) eventDispatcher;
 @end
